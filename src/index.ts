8c91b4f481ee6d7aafed1914921ec055afd51330
Gott50 <xxGott50xx@gmail.com>
2018-03-19 14:23:29 +0100

    setup Express

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.set('port', process.env.PORT || 5000);
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid
+// certificate authority.
+app.listen(app.get('port'), () => {
+    console.log('Node app is running on port', app.get('port'));
+});
